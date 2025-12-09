9dec80cd5fb62177f8c2375e1c83a0d62a1ae6ce
JOSE MOISES NUÑEZ IGLESIAS <numezmoises1@gmail.com>
2025-12-09 01:08:21 -0600

    feat: frozen card

--- app/screens/card-dashboard/card-dashboard-screen.tsx
+++ app/screens/card-dashboard/card-dashboard-screen.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Icon, makeStyles, Text, useTheme } from "@rn-vui/themed"
-import { ScrollView, View } from "react-native"
+import { ScrollView, View, Image } from "react-native"
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
@@ -10,7 +10,7 @@ import { VisaCard } from "../../components/visa-card/visa-card"
 import { testProps } from "@app/utils/testProps"
 import { IconNamesType } from "@app/components/atomic/galoy-icon"
 import { GaloyIconButton } from "@app/components/atomic/galoy-icon-button"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const CardDashboardScreen: React.FC = () => {
   const styles = useStyles()
@@ -19,9 +19,21 @@ export const CardDashboardScreen: React.FC = () => {
   } = useTheme()
   const { LL } = useI18nContext()
   const navigation = useNavigation<StackNavigationProp<CardDashboardStackParamList>>()
+  const [isFrozen, setIsFrozen] = useState(false)
 
   useEffect(() => {
     navigation.setOptions({
+      headerLeft: () => (
+        <GaloyIconButton
+          style={styles.headerLeft}
+          name={"arrow-left"}
+          size="medium"
+          iconOnly
+          color={colors.black}
+          onPress={() => navigation.navigate("cardDashboardScreen")}
+        />
+      ),
+
       headerRight: () => (
         <GaloyIconButton
           style={styles.headerRight}
@@ -35,37 +47,38 @@ export const CardDashboardScreen: React.FC = () => {
     })
   }, [navigation, styles])
 
-  type Target =
-    | "scanningQRCode"
-    | "sendBitcoinDestination"
-    | "receiveBitcoin"
-    | "transactionHistory"
+  const onMenuClick = (target: Target) => {
+    navigation.navigate(target)
+  }
+
+  type Target = "cardDashboardScreen"
 
   const buttons = [
     {
+      id: "1",
       title: "Details",
-      target: "receiveBitcoin" as Target,
+      event: () => onMenuClick("cardDashboardScreen"),
       icon: "eye" as IconNamesType,
     },
     {
+      id: "2",
       title: "Freeze",
-      target: "sendBitcoinDestination" as Target,
+      event: () => setIsFrozen(!isFrozen), //Mock
       icon: "freeze" as IconNamesType,
     },
     {
+      id: "3",
       title: "Set limits",
-      target: "scanningQRCode" as Target,
+      event: () => onMenuClick("cardDashboardScreen"),
       icon: "limit" as IconNamesType,
     },
     {
+      id: "4",
       title: "Statements",
-      target: "scanningQRCode" as Target,
+      event: () => onMenuClick("cardDashboardScreen"),
       icon: "book" as IconNamesType,
     },
   ]
-  const onMenuClick = (target: Target) => {
-    navigation.navigate(target as any)
-  }
 
   return (
     <Screen>
@@ -73,13 +86,38 @@ export const CardDashboardScreen: React.FC = () => {
         {...testProps("card-dashboard")}
         contentContainerStyle={styles.scrollViewContainer}
       >
-        <VisaCard
-          expiredDate={".. / .."}
-          name={"satoshi nakamoto"}
-          cardNumber=".... .... .... 2121"
-          useGradient
-          gradientDegrees={45}
-        />
+        <View style={styles.cardContainer}>
+          <VisaCard
+            expiredDate={".. / .."}
+            name={"satoshi nakamoto"}
+            cardNumber=".... .... .... 2121"
+            useGradient
+            gradientDegrees={45}
+          />
+          {isFrozen && (
+            <View style={styles.blurOverlay}>
+              <Image
+                source={require("../../assets/images/blur-card.png")}
+                resizeMode="cover"
+                style={{ width: "103%", height: "103%" }}
+              />
+              <View style={styles.frozenContent}>
+                <Icon
+                  type="ionicon"
+                  name="lock-closed-outline"
+                  color={colors.error}
+                  backgroundColor={"#DC262633"}
+                  style={{ padding: 12, borderRadius: 50 }}
+                  size={40}
+                />
+                <Text type="h2" bold style={{ fontWeight: "bold" }}>
+                  Card frozen
+                </Text>
+                <Text type="p4">Card is temporarily disabled</Text>
+              </View>
+            </View>
+          )}
+        </View>
         <View style={styles.balance}>
           <View>
             <Text type="p1">$21.21</Text>
@@ -87,7 +125,7 @@ export const CardDashboardScreen: React.FC = () => {
               ~ Kč500.00
             </Text>
           </View>
-          <View style={styles.addButton}>
+          <View style={[styles.addButton, isFrozen ? styles.addButtonDisabled : {}]}>
             <Text type="p3">Add funds</Text>
             <Icon type="ionicon" name="add-outline" color={colors.primary} size={20} />
           </View>
@@ -99,7 +137,9 @@ export const CardDashboardScreen: React.FC = () => {
                 name={item.icon}
                 size="large"
                 text={item.title}
-                onPress={() => onMenuClick(item.target)}
+                onPress={() => item.event()}
+                color={isFrozen && item.id == "2" ? colors.error : undefined}
+                backgroundColor={isFrozen && item.id == "2" ? colors.error9 : undefined}
               />
             </View>
           ))}
@@ -148,13 +188,43 @@ const useStyles = makeStyles(({ colors }) => ({
     justifyContent: "center",
     gap: 10,
   },
+  addButtonDisabled: {
+    opacity: 0.5,
+  },
   noTransaction: {
     alignItems: "center",
     alignSelf: "center",
     marginTop: 20,
     color: colors.grey2,
   },
+  headerLeft: {
+    marginLeft: 12,
+  },
   headerRight: {
     marginRight: 12,
+  },
+  cardContainer: {
+    position: "relative",
+    overflow: "hidden",
+  },
+  blurOverlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  frozenContent: {
+    position: "absolute",
+    backgroundColor: "rgba(0, 0, 0, 0.58)",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    gap: 8,
+    flex: 1,
+    justifyContent: "center",
   },
 }))
